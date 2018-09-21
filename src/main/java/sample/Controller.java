70bb43013af6f442ee4456d97c3a90e9ea653b2e
Daniel White <whitedann@gmail.com>
2018-09-20 21:56:48 -0700

    added master control downloader and plate data copy method

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,15 +1,19 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import sample.elements.WellState;
 import sample.excelFiles.OutputSheet;
 
 import java.io.IOException;
 
 import static javafx.scene.paint.Color.BLACK;
+import static javafx.scene.paint.Color.ORANGE;
 
 
 public class Controller {
@@ -18,44 +22,85 @@ public class Controller {
     @FXML
     private Pane plateGrid;
 
+    @FXML
+    private HBox bottomPane;
+    @FXML
+    private HBox bottomBPane;
+
+    private Text upThresh = new Text("Upper Threshold: ");
+    private Text lowThresh = new Text("Lower Threshold: ");
+    private Text wellPos = new Text("Well Position:");
+    private Text targetVol = new Text("Target Volume: ");
+    private Circle focusImage = new Circle(0,0,0);
+
     private Circle[][] wells = new Circle[12][8];
-    OutputSheet data = new OutputSheet("/assets/template.xlsx");
+    OutputSheet outputSheet = new OutputSheet();
 
     public Controller() throws IOException {
-
     }
-
 
     @FXML
     protected void initialize(){
+        targetVol.setText("Target Volume: ");
+        bottomPane.setPadding(new Insets(0, 21, 0, 8));
+        bottomPane.setSpacing(100);
+        bottomBPane.setPadding(new Insets(0, 0, 0, 0));
+        bottomBPane.setSpacing(100);
+        bottomPane.getChildren().add(wellPos);
+        bottomBPane.getChildren().add(targetVol);
+        bottomBPane.getChildren().add(lowThresh);
+        bottomPane.getChildren().add(upThresh);
+
         for(int j = 0; j < wells[0].length; j++) {
             for(int i = 0; i < wells.length; i++) {
                 Circle newCircle = new Circle(44 * i + 24, 44*j + 24, 22);
-                newCircle.setStyle("-fx-fill: dodgerblue");
+                newCircle.setStyle("-fx-fill: white");
                 newCircle.setStroke(BLACK);
-                int finalI = i + 1;
-                int finalJ = j + 1;
+                int finalI = i;
+                int finalJ = j;
                 newCircle.setOnMouseClicked(e -> {
                     System.out.println(finalI + " " + finalJ);
+                    setFocus(finalI, finalJ);
                 });
                 wells[i][j] = newCircle;
                 plateGrid.getChildren().add(wells[i][j]);
             }
         }
+        plateGrid.getChildren().add(focusImage);
+    }
+
+    private void setFocus(int row, int col) {
+        this.targetVol.setText(String.format("Target Volume: %3.1f",outputSheet.getTargetVolume(row, col)));
+        this.wellPos.setText(String.format("Well Position: " + outputSheet.getWellPosition(row, col)));
+        this.upThresh.setText(String.format("Upper Threshold: ") + outputSheet.getUpperThreshold(row, col));
+        this.lowThresh.setText(String.format("Lower Threshold: " + outputSheet.getLowerThreshold(row, col)));
+        focusImage.setCenterY(44*(col)+24);
+        focusImage.setCenterX(44*(row)+24);
+        focusImage.setRadius(21);
+        focusImage.setFill(ORANGE);
     }
 
     @FXML
-    private void updateStatus(){
-        WellState[][] states = data.getResultsArray();
+    private void updateStatus() throws IOException {
+        outputSheet.parseResult();
+        WellState[][] states = outputSheet.getResultsArray();
         for(int j = 0; j < wells[0].length; j++){
             for(int i = 0; i < wells.length; i++){
                 if(states[i][j].equals(WellState.PASS)) {
                     wells[i][j].setStyle("-fx-fill: green");
                 }
+                else if(states[i][j].equals(WellState.NODATA)){
+                    wells[i][j].setStyle("-fx-fill: lightgrey");
+                }
                 else
                     wells[i][j].setStyle("-fx-fill: red");
             }
         }
+    }
+
+    @FXML
+    private void loadSheet() throws IOException {
+        outputSheet.prepareDocument();
     }
 
     @FXML
--- src/main/java/sample/excelFiles/OutputSheet.java
+++ src/main/java/sample/excelFiles/OutputSheet.java
@@ -1,57 +1,159 @@
 package sample.excelFiles;
 
+import javafx.scene.shape.Circle;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import sample.elements.WellState;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class OutputSheet {
 
-    private FileInputStream fileStream;
-    private Workbook wb;
-    private Sheet resultSheet, dataSheet;
-    String[][] data = null;
+    private Workbook template, plateVolumeInfo;
+    private Sheet resultSheet, dataSheet, plateData;
+    double[][] targetData = null;
+    double[][] highEnds = null;
+    double[][] lowEnds = null;
+    String[][] wellPositions = null;
     WellState[][] states = null;
 
-    public OutputSheet(String path) throws IOException {
-        this.setInputFile(path);
-        parseResult();
+    public OutputSheet() throws IOException {
     }
 
-    private void setInputFile(String path) throws IOException {
-        File file = new File("./src/main/resources/assets/template.xlsx");
-        System.out.println(file.getAbsolutePath());
-        fileStream = new FileInputStream(file);
-        wb = new XSSFWorkbook(fileStream);
-        resultSheet = wb.getSheetAt(0);
-        dataSheet = wb.getSheetAt(1);
-        data = new String[12][8];
-        states = new WellState[12][8];
-        /**for(File e : file.listFiles()) {
-            System.out.println(e + " " + e.canRead());
-        }**/
-    }
+    private void downloadTemplate() throws IOException {
+        String url = "https://idtdna.mastercontrol.com/mc/login/index.cfm?action=login";
 
-    public void parseResult(){
-        for(int i = 0; i < 96; i++){
-            int col = i % 12;
-            int row = i / 12;
-            data[col][row] = resultSheet.getRow(i+3).getCell(3).getStringCellValue();
-            if(data[col][row].equals("PASS"))
-                states[col][row] = WellState.PASS;
-            else
-                states[col][row] = WellState.FAIL;
+        BasicCookieStore store = new BasicCookieStore();
+        CloseableHttpClient client = HttpClients.custom().setDefaultCookieStore(store).build();
+
+        try {
+            HttpUriRequest login = RequestBuilder.post()
+                    .setUri(url)
+                    .addParameter("username", "dwhite")
+                    .addParameter("username2", "")
+                    .addParameter("initialRequest", "")
+                    .addParameter("password", "Pwner4509!")
+                    .build();
+            CloseableHttpResponse response2 = client.execute(login);
+            try{
+                HttpEntity entity = response2.getEntity();
+                System.out.println("Login form get: " + response2.getStatusLine());
+                EntityUtils.consume(entity);
+            } finally {
+                response2.close();
+            }
+
+            String downloadLink = "https://idtdna.mastercontrol.com/mc/Main/MASTERControl/Organizer/view_file.cfm?id=XBCYSHLULRA4XBIBB7";
+            HttpGet get2 = new HttpGet(downloadLink);
+            CloseableHttpResponse response3 = client.execute(get2);
+
+            try{
+                HttpEntity entity = response3.getEntity();
+                System.out.println("Status: " + response3.getStatusLine());
+                BufferedInputStream bis = new BufferedInputStream(entity.getContent());
+                if(bis.available() == 1){
+                    System.out.println("Could not connect to Master Control");
+                    bis.close();
+                    return;
+                }
+                BufferedOutputStream bos = new BufferedOutputStream( new FileOutputStream(new File("./cool.xlsx")));
+                int inByte;
+                while((inByte = bis.read()) != -1)
+                    bos.write(inByte);
+                EntityUtils.consume(entity);
+                bos.close();
+                bis.close();
+            }
+            finally {
+                response3.close();
+            }
+        }
+        finally {
+            client.close();
         }
     }
 
-    public void printResults(){
-        for(int j = 0; j < data.length; j++){
-            for(int i = 0; i < data[0].length; i++){
-                System.out.println(data[j][i] + " row: " + (j+1) + " col: " + (i+1));
+    private void downloadPlateVolumeInfo() {
+        /** TODO: Make this! **/
+        for(int i = 3; i < 99; i++){
+            resultSheet.getRow(i).getCell(2).setCellValue(100);
+        }
+    }
+
+    public void prepareDocument() throws IOException {
+        downloadTemplate();
+        setInputFile();
+        downloadPlateVolumeInfo();
+        mergeTemplateWithPlateVolume();
+        saveFinalSheet();
+    }
+
+    private void setInputFile() throws IOException {
+        template = new XSSFWorkbook(new FileInputStream(new File("./cool.xlsx")));
+        dataSheet = template.getSheetAt(1);
+        resultSheet = template.getSheetAt(0);
+
+        plateVolumeInfo = new XSSFWorkbook(new FileInputStream(new File("./src/main/resources/assets/plateVol.xlsx")));
+        plateData = plateVolumeInfo.getSheetAt(0);
+
+        states = new WellState[12][8];
+        targetData = new double[12][8];
+        highEnds = new double[12][8];
+        lowEnds = new double[12][8];
+        wellPositions = new String[12][8];
+    }
+
+    private void mergeTemplateWithPlateVolume(){
+        dataSheet.setForceFormulaRecalculation(true);
+        for(int i = 2; i < plateData.getPhysicalNumberOfRows(); i++){
+            for(int j = 1; j <= 4; j++) {
+                dataSheet.getRow(i-2).getCell(0).setCellValue(plateData.getRow(i).getCell(0).getStringCellValue());
+                dataSheet.getRow(i-2).getCell(j).setCellValue(plateData.getRow(i).getCell(j).getNumericCellValue());
+            }
+            dataSheet.getRow(i-2).createCell(5);
+            dataSheet.getRow(i-2).getCell(5).setCellValue(plateData.getRow(i).getCell(5).getNumericCellValue());
+        }
+
+    }
+
+    private void saveFinalSheet() throws IOException {
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        String fileLocation = path.substring(0, path.length() - 1) + "temp1.xlsx";
+        BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(fileLocation));
+        XSSFFormulaEvaluator.evaluateAllFormulaCells(template);
+        template.write(outputStream);
+        outputStream.close();
+    }
+
+    public void parseResult() throws IOException {
+        for(int i = 0; i < 96; i++){
+            int col = i % 12;
+            int row = i / 12;
+            double targetVol = resultSheet.getRow(i+3).getCell(2).getNumericCellValue();
+            targetData[col][row] = dataSheet.getRow(i).getCell(3).getNumericCellValue();
+            highEnds[col][row] = targetData[col][row] * 1.05 + 10;
+            lowEnds[col][row] = targetData[col][row] * 0.95 - 10;
+            wellPositions[col][row] = resultSheet.getRow(i+3).getCell(0).getStringCellValue();
+            if(targetData[col][row] == 0){
+                states[col][row] = WellState.NODATA;
+            }
+            else if(targetVol > highEnds[col][row] || targetVol < lowEnds[col][row]) {
+                states[col][row] = WellState.FAIL;
+            }
+            else {
+                states[col][row] = WellState.PASS;
             }
         }
     }
@@ -59,5 +161,22 @@ public class OutputSheet {
     public WellState[][] getResultsArray(){
         return this.states;
     }
+
+    public String getWellPosition(int col, int row){
+        return wellPositions[col][row];
+    }
+
+    public double getTargetVolume(int col, int row){
+        return targetData[col][row];
+    }
+
+    public double getUpperThreshold(int col, int row){
+        return highEnds[col][row];
+    }
+
+    public double getLowerThreshold(int col, int row){
+        return lowEnds[col][row];
+    }
+
 
 }
