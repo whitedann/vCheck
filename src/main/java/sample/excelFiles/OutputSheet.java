e0d01eb0bad78770d9853eeaef4ca35c0357b77a
Daniel White <whitedann@gmail.com>
2018-10-12 16:05:31 -0700

    fixed bug with template merge method when the total number of well exceeds 96 (like invitae plates)

--- src/main/java/sample/excelFiles/OutputSheet.java
+++ src/main/java/sample/excelFiles/OutputSheet.java
@@ -237,9 +237,9 @@ public class OutputSheet {
         CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
         List<Double> tempValList = new ArrayList<>();
         for(CSVRecord csvRecord : csvParser){
-            double val = Double.parseDouble(csvRecord.get(4));
-            System.out.println(val);
-            tempValList.add(val);
+            //double val = Double.parseDouble(csvRecord.get(4));
+            System.out.println(csvRecord.get(4));
+            //tempValList.add(val);
         }
         for(int i = 0; i < tempValList.size(); i++){
             int row = i / 12;
